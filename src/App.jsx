94a20886912cf9eb3162e121a7f2fdfb27967b3d
anmol938 <anmol938@gmail.com>
2023-10-11 19:06:01 +0530

    v2 + bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello from V2</h1>
+                <h1>Hello from V2 and holla</h1>
             </header>
         </div>
     );
